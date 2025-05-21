7c2c3c22619740e7e434b2e3328121acb0dc472c
Ignacy Dudziak <ignacydudziak@gmail.com>
2025-05-21 11:56:45 +0200

    2025-05-21 ID

--- app/views.py
+++ app/views.py
@@ -2,10 +2,12 @@ from app import app
 import os
 import json
 import requests
+import pandas as pd
 from flask import render_template, request, redirect, url_for
 from config import headers
 from bs4 import BeautifulSoup
 from app import utils
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -18,6 +20,17 @@ def display_form():
 def extract():
     product_id = request.form.get("product_id")
     next_page = (f"https://www.ceneo.pl/{product_id}#tab=reviews")
+    response = requests.get(next_page,headers=headers)
+    if response.status_code == 200:
+        page_dom = BeautifulSoup(response.text, "html.parser")
+        product_name = utils.extract_feature(page_dom, "h1")
+        opinions_count = utils.extract_feature(page_dom, "a.product-review__link > span")
+        if not opinions_count:
+            error="Dla produktu o podanym id nie ma jeszcze żadnych opinii"
+            return render_template("extract.html", error=error)
+    else:
+        error="Nie znaleziono produktu o podanym id"
+        return render_template("extract.html", error=error)
     all_opinions = []
     while next_page:
         response = requests.get(next_page,headers=headers)
@@ -31,7 +44,7 @@ def extract():
                 }
                 all_opinions.append(single_opinion)
             try:
-                next_page = "https://www.ceneo.pl" + extract(page_dom,"a.pagination__next","href")
+                next_page = "https://www.ceneo.pl" + utils.extract_feature(page_dom,"a.pagination__next","href")
             except TypeError:
                 next_page = None
                 print("Brak kolejnej strony.")
@@ -41,10 +54,10 @@ def extract():
         os.mkdir("./app/data")
     if not os.path.exists("./app/data/opinions"):
         os.mkdir("./app/data/opinions")
-    with open(f"./app/data/opinions{product_id}.json", "w", encoding="UTF-8") as jf:
+    with open(f"./app/data/opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
         json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
 
-    return redirect(url_for("product", product_id=product_id))
+    return redirect(url_for("product", product_id=product_id, product_name=product_name))
 
 @app.route("/products")
 def products():
@@ -56,4 +69,6 @@ def author():
 
 @app.route("/product/<product_id>")
 def product(product_id):
-    return render_template("product.html", product_id=product_id)
+    product_name = request.args.get("product_name")
+    opinions = pd.read_json(f"./app/data/opinions/{product_id}.json")
+    return render_template("product.html", product_id=product_id, product_name=product_name, opinions=opinions.to_html(classes="display", table_id='opinions'))
